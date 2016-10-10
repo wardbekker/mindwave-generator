a8336b8671a42ca72da6796d03a316a40b3611ae
Ward Bekker <wbekker@HW10023.local>
2016-10-10 16:46:54 +0200

    First commit of timeseries generator

---
+++ generate.rb
@@ -0,0 +1,28 @@
+require 'socket'
+require 'json'
+
+server = TCPServer.open 2000
+puts "Listening on port 2000"
+
+
+client = server.accept()
+
+loop {
+
+  params = {
+    timestamp: (Time.now.to_f * 1000).to_i, #milliseconds since epoch
+    delta: rand(1..100),
+    theta: rand(1..100),
+    low_alpha: rand(1..100),
+    high_alpha: rand(1..100),
+    low_beta: rand(1..100),
+    high_beta: rand(1..100),
+    low_gamma: rand(1..100),
+    mid_gamma: rand(1..100),
+    attention: rand(1..100),
+    meditation: rand(1..100)
+  }
+
+  client.puts params.to_json
+  sleep 0.1
+}
