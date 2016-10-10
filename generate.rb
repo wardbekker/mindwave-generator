260135e44cf088d9d070f678e60ecd1c6cc6f7ed
Ward Bekker <wbekker@HW10023.local>
2016-10-10 18:48:27 +0200

    Now accepting connections from multiple clients

--- generate.rb
+++ generate.rb
@@ -1,30 +1,24 @@
 require 'socket'
 require 'json'
 
-server = TCPServer.open 2000
-puts "Listening on port 2000"
-
+s = TCPSocket.new 'localhost', 2000
 
 loop {
-  Thread.start(server.accept) do |client|
-    puts "client connected"
-    loop {
-      params = {
-        timestamp: (Time.now.to_f * 1000).to_i, #milliseconds since epoch
-        delta: rand(1..100),
-        theta: rand(1..100),
-        low_alpha: rand(1..100),
-        high_alpha: rand(1..100),
-        low_beta: rand(1..100),
-        high_beta: rand(1..100),
-        low_gamma: rand(1..100),
-        mid_gamma: rand(1..100),
-        attention: rand(1..100),
-        meditation: rand(1..100)
-      }
-      
-      client.puts params.to_json
-      sleep 0.1      
-    }
-  end
+  puts "connected, sending json.."
+  params = {
+    timestamp: (Time.now.to_f * 1000).to_i, #milliseconds since epoch
+    delta: rand(1..100),
+    theta: rand(1..100),
+    low_alpha: rand(1..100),
+    high_alpha: rand(1..100),
+    low_beta: rand(1..100),
+    high_beta: rand(1..100),
+    low_gamma: rand(1..100),
+    mid_gamma: rand(1..100),
+    attention: rand(1..100),
+    meditation: rand(1..100)
+  }
+  
+  client.puts params.to_json
+  sleep 0.1
 }
